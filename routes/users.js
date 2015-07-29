a5d88218e7b0656bcc301556cd21a03ba4e6f8fb
Rachel <rachella@gmail.comOB>
2015-07-29 14:22:59 +0800

    feat(getting-started): Add password encryption for user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+var Bcrypt = require('bcrypt');
+
 //Defining the plugin
 exports.register = function(server, options, next){
   //Define routes
@@ -9,22 +11,53 @@ exports.register = function(server, options, next){
         path: '/users',
         handler: function(request, reply){
           var db = request.server.plugins['hapi-mongodb'].db;
-
           var user = request.payload.user;
           // user = {
           //    name: ...,
           //    email: ...
           //}
           
-          db.collection('users').insert(user, function(err, writeResult){
-            reply(writeResult);
-            //with the function because waits for collection to be successful before replying.
-          });
+          // db.collection('users').insert(user, function(err, writeResult){
+          //   reply(writeResult);
+          //   //with the function because waits for collection to be successful before replying.
+          // });
+          //or operator is if there is multiple fields
+          var unqiUserQuery = {
+            $or: [
+              {username: user.username},
+              {email: user.email}
+            ]
+          };
+
+          //encrypt the password
+          Bcrypt.genSalt(10, function(err, salt){
+            Bcrypt.hash(user.password, salt,function(err, encrypted){
+              user.password = encrypted;
+              //inserting a user doc into the DB
+              //if unqiuserquery is true then userExist will return. First parameter is always an error. use return in if statement so no need for else.
+              db.collection('users').count(unqiUserQuery, function(err, userExist){
+                //see if it request reply(result aka userExist)
+                // reply(userExist);
+                if (userExist) {
+                  return reply("Error. Username already exists");
+                }
+
+                db.collection('users').insert(user, function(err, writeResult){
+                  if (err) {return reply("Internal MongoDB error", err)}
+
+                  reply(writeResult);
+                })
+              });
+
+              
+            })
+          })
+
         }
       }
     ]);
 
-  next(); //<= don't forget this
+  next(); //<= DO NOT FORGET THIS!
 };
 
 //Defining the decription of the plugin
